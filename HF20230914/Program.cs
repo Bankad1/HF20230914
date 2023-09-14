0d99b20dc8b60d4d9293682cfc68c383125c603f
banka <banka@DESKTOP-Q4L3KPD>
2023-09-14 10:34:35 +0200

    Add project files.

---
+++ HF20230914/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HF20230914
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
