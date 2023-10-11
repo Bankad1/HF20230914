b6d2d9affd132da94fe0f777c75059b7f6548432
Bankad1 <75490486+Bankad1@users.noreply.github.com>
2023-10-11 17:41:35 +0200

    Update Program.cs

--- HF20230914/Program.cs
+++ HF20230914/Program.cs
@@ -1,194 +1,43 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
 
-namespace DominoBacktrack
+class Knapsack
 {
-    public struct Separator
+    static int KnapsackDP(int[] weights, int[] values, int capacity)
     {
-        private int cell1, cell2;
+        int n = weights.Length;
+        int[,] dp = new int[n + 1, capacity + 1];
 
-        public Separator(int cell1, int cell2)
+        // Build the knapsack table
+        for (int i = 0; i <= n; i++)
         {
-            this.cell1 = cell1;
-            this.cell2 = cell2;
-        }
-
-        public Separator(int[] cells)
-        {
-            if (cells.Length != 2)
-                throw new ArgumentException("Length of array is not 2.", "cells");
-
-            this.cell1 = cells[0];
-            this.cell2 = cells[1];
-        }
-
-        public int this[int index]
-        {
-            get
+            for (int w = 0; w <= capacity; w++)
             {
-                if (index == 0)
-                    return this.cell1;
-                else if (index == 1)
-                    return this.cell2;
+                if (i == 0 || w == 0)
+                {
+                    dp[i, w] = 0;
+                }
+                else if (weights[i - 1] <= w)
+                {
+                    dp[i, w] = Math.Max(values[i - 1] + dp[i - 1, w - weights[i - 1]], dp[i - 1, w]);
+                }
                 else
-                    throw new IndexOutOfRangeException("Index must be 0 or 1");
-            }
-            set
-            {
-                if (index == 0)
-                    this.cell1 = value;
-                else if (index == 1)
-                    this.cell2 = value;
-                else
-                    throw new IndexOutOfRangeException("Index must be 0 or 1");
+                {
+                    dp[i, w] = dp[i - 1, w];
+                }
             }
         }
+
+        // The result is stored in dp[n, capacity]
+        return dp[n, capacity];
     }
 
-    public enum Orientation
+    static void Main()
     {
-        Horizontal = 0,
-        Vertical = 1
-    }
+        int[] weights = { 1, 2, 3, 4, 5 };
+        int[] values = { 10, 20, 30, 40, 50 };
+        int capacity = 7;
 
-    internal class Program
-    {
-        static int attemptCounter = 0;
-
-        static void ReadInput(string inputFilePath, out int rows, out int columns, out Separator[] separators)
-        {
-            using (StreamReader reader = new StreamReader(inputFilePath))
-            {
-                string[] firstLine = reader.ReadLine().Split(' ');
-                rows = Convert.ToInt32(firstLine[0]);
-                columns = Convert.ToInt32(firstLine[1]);
-
-                int numSeparators = Convert.ToInt32(reader.ReadLine());
-                separators = new Separator[numSeparators];
-
-                for (int i = 0; i < numSeparators; i++)
-                {
-                    string[] line = reader.ReadLine().Split(' ');
-                    separators[i] = new Separator(Convert.ToInt32(line[0]), Convert.ToInt32(line[1]));
-                }
-            }
-        }
-
-        static bool IsValidPlacement(int[] vector, int index, int orientation, int rows, int columns)
-        {
-            if (orientation == (int)Orientation.Horizontal)
-            {
-                return index % columns < columns - 1 &&
-                       vector[index] == 0 &&
-                       vector[index + 1] == 0;
-            }
-            else
-            {
-                return index / columns < rows - 1 &&
-                       vector[index] == 0 &&
-                       vector[index + columns] == 0;
-            }
-        }
-
-        static bool IsSeparatorBlocked(int[,] solution, Separator separator)
-        {
-            for (int j = 0; j < solution.GetLength(0); j++)
-            {
-                if (solution[j, 0] == separator[0] &&
-                    ((Orientation)solution[j, 1] == Orientation.Horizontal) &&
-                    solution[j, 0] + 1 == separator[1] ||
-                    (Orientation)solution[j, 1] == Orientation.Vertical &&
-                    solution[j, 0] + solution.GetLength(1) == separator[1])
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        static bool SolveDominoProblem(int[] vector, int[,] solution, Separator[] separators, int index, int rows, int columns)
-        {
-            if (index >= vector.Length)
-                return true;
-
-            attemptCounter++;
-
-            for (int orientation = 0; orientation <= 1; orientation++)
-            {
-                if (IsValidPlacement(vector, index, orientation, rows, columns))
-                {
-                    vector[index] = 1;
-                    int emptyIndex = -1;
-
-                    for (int i = index + 1; i < vector.Length; i++)
-                    {
-                        if (vector[i] == 0)
-                        {
-                            emptyIndex = i;
-                            break;
-                        }
-                    }
-
-                    if (emptyIndex != -1)
-                    {
-                        vector[emptyIndex] = 1;
-                        solution[index / 2, 0] = emptyIndex;
-                        solution[index / 2, 1] = orientation;
-
-                        bool blocked = false;
-                        foreach (var separator in separators)
-                        {
-                            if (IsSeparatorBlocked(solution, separator))
-                            {
-                                blocked = true;
-                                break;
-                            }
-                        }
-
-                        if (!blocked && SolveDominoProblem(vector, solution, separators, emptyIndex + 1, rows, columns))
-                            return true;
-
-                        vector[emptyIndex] = 0;
-                    }
-
-                    vector[index] = 0;
-                }
-            }
-
-            return false;
-        }
-
-        static void Main(string[] args)
-        {
-            int rows, columns;
-            Separator[] separators;
-            ReadInput("input_3.txt", out rows, out columns, out separators);
-
-            int[] vector = new int[rows * columns];
-            int[,] solution = new int[rows * columns / 2, 2];
-
-            if (SolveDominoProblem(vector, solution, separators, 0, rows, columns))
-            {
-                using (StreamWriter writer = new StreamWriter("solution.txt"))
-                {
-                    writer.WriteLine("A solution is:");
-                    writer.WriteLine(new string('—', 7));
-                    for (int k = 0; k < solution.GetLength(0); k++)
-                    {
-                        writer.WriteLine("| {0} {1} |", solution[k, 0], (Orientation)solution[k, 1] == Orientation.Horizontal ? solution[k, 0] + 1 : solution[k, 0] + columns);
-                    }
-                    writer.WriteLine(new string('—', 7));
-                }
-
-                Console.WriteLine($"Solution found in {attemptCounter} attempts.");
-            }
-            else
-            {
-                Console.WriteLine("No solution found.");
-            }
-
-            Console.ReadLine();
-        }
+        int maxValue = KnapsackDP(weights, values, capacity);
+        Console.WriteLine("Maximum value that can be obtained: " + maxValue);
     }
 }
